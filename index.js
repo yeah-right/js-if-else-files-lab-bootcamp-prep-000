ad1e22177cc8acf89d9d5080a7f4c7025fa05496
Zack A Claar <claar.zack@gmail.com>
2019-02-16 06:27:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-var name = "Susan"
+var name = "Joe"
 var height = "74"
 
 
 // Don't worry about this
 module.exports = { name, height
-}
-
-if (name === "Susan") {
-    console.log("The name is correct")
-} else {
-    console.log("Expected the name to be Susan")
 }
